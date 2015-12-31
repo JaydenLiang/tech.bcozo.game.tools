f2fed9a251279e204378c471d08bb321e4da06cd
Jayden Liang <jaydenliang81@gmail.com>
2015-12-31 12:10:51 -0800

    Add new interface
    Add callback handle interface

---
+++ core/src/tech/bcozo/game/tools/logic/ICallbackHandler.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package tech.bcozo.game.tools.logic;
+
+/**
+ * <p>
+ * Javadoc description
+ * </p>
+ * 
+ * @ClassName: ICallbackHandler
+ * @author Jayden Liang
+ * @version 1.0
+ * @date Dec 26, 2015 1:02:14 PM
+ */
+public interface ICallbackHandler {
+    public void callback(Object callObject) throws Exception;
+}
