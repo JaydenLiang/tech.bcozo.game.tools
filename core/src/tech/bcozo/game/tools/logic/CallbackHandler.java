e1c69be47b76659999e66420e7416a73ada7d75c
Jayden Liang <jaydenliang81@gmail.com>
2015-12-31 12:12:31 -0800

    Add callback handle
    Add a new callback class.

---
+++ core/src/tech/bcozo/game/tools/logic/CallbackHandler.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package tech.bcozo.game.tools.logic;
+
+/**
+ * <p>
+ * Javadoc description
+ * </p>
+ * 
+ * @ClassName: CallbackHandler
+ * @author Jayden Liang
+ * @version 1.0
+ * @date Dec 26, 2015 1:12:15 PM
+ */
+public class CallbackHandler implements ICallbackHandler {
+
+    /**
+     * <p>
+     * This is the constructor of CallbackHandler
+     * </p>
+     */
+    public CallbackHandler() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * tech.bcozo.game.tools.logic.ICallbackHandler#callback(java.lang.Object)
+     */
+    @Override
+    public void callback(Object callObject) {
+
+    }
+
+}
